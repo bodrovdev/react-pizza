f369d845735f3c0aef5469d72387dd848191b97c
bodrovdev <bodrovigor02@gmail.com>
2024-02-28 12:55:37 +0300

    feat: 🎸 axios, debounce, fixes

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,24 +1,52 @@
+import { useState, useRef, useCallback } from 'react';
 import '../../scss/style.scss';
 import styles from './Search.module.scss';
-
-import Clean from '../Icons/Clean';
-import SearchIcon from '../Icons/SearchIcon';
+import debounce from 'lodash.debounce';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchValue } from '../../redux/slices/searchSlice';
 
-function Search() {
+import Clean from '../Icons/Clean';
+import SearchIcon from '../Icons/SearchIcon';
 
-  const searchValue = useSelector((state) => state.search.value);
+function Search() {
+  const inputRef = useRef();
+
   const dispatch = useDispatch();
+
+  const [localSearch, setLocalSearch] = useState('');
+
+  const updateSearchValue = useCallback(
+    debounce((localSearchValue) => {
+      dispatch(setSearchValue(localSearchValue));
+    }, 500), []
+  )
+
+  const onChangeInput = (e) => {
+    setLocalSearch(e.target.value);
+    updateSearchValue(localSearch);
+  }
+
+  const onClean = () => {
+    setLocalSearch('');
+    dispatch(setSearchValue(''));
+    inputRef.current.focus();
+  }
 
   return (
     <div className={styles.searchBlock}>
       <SearchIcon className={styles.searchIcon} />
 
-      <input className={styles.searchInput} onChange={(e) => { dispatch(setSearchValue(e.target.value)) }} value={searchValue} type="text" placeholder="Поиск" />
+      <input
+        className={styles.searchInput}
+        type="text"
+        placeholder="Поиск"
+        value={localSearch}
+        onChange={(e) => { onChangeInput(e) }}
+        ref={inputRef}
+      />
 
-      {searchValue && <Clean className={styles.searchClean} onClickClean={() => { dispatch(setSearchValue('')) }} />}
+      {localSearch && <Clean className={styles.searchClean} onClickClean={() => { onClean() }} />}
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import '../../scss/style.scss';
 import styles from './Home.module.scss';
 
@@ -9,10 +10,7 @@ import Sort from '../../components/Sort';
 import { useSelector } from 'react-redux';
 
 function Home() {
-
-  const categoryValue = useSelector((state) => state.sort.categoryValue);
-  const sortValue = useSelector((state) => state.sort.sortValue);
-  const sortDir = useSelector((state) => state.sort.sortDir);
+  const { categoryValue, sortValue, sortDir } = useSelector((state) => state.sort);
   const searchValue = useSelector((state) => state.search.value);
 
   //* pizza items
@@ -22,19 +20,19 @@ function Home() {
   // * url
   const getCategory = categoryValue === 0 ? `` : `category=${categoryValue}`;
   const getSort = `&sortBy=${sortValue.sortType}&order=${sortDir === false ? 'desc' : 'asc'}`;
+  const getSearch = searchValue && `&search=${searchValue}`;
 
   useEffect(() => {
     setLoading(true);
 
-    let url = `https://653e4e07f52310ee6a9acea3.mockapi.io/items?${getCategory}${getSort}`;
+    let url = `https://653e4e07f52310ee6a9acea3.mockapi.io/items?${getCategory}${getSearch}${getSort}`;
 
-    fetch(url)
-      .then((data) => data.json())
-      .then((data) => {
-        setPizzasItems(data);
-        setLoading(false);
-      });
-  }, [categoryValue, sortValue]);
+    axios.get(url).then((res) => {
+      setPizzasItems(res.data);
+      setLoading(false);
+    })
+
+  }, [categoryValue, sortValue, searchValue]);
 
   return (
     <>
@@ -54,7 +52,7 @@ function Home() {
             </div> :
             <div className={styles.wrapper}>
 
-              {pizzasItems.filter(item => (item.name.toLowerCase().includes(searchValue.toLowerCase()))).map((item, index) => (
+              {pizzasItems.map((item, index) => (
 
                 <PizzaBlock imageUrl={item.imageUrl} name={item.name} types={item.types} sizes={item.sizes} price={item.price} key={index} />
 
